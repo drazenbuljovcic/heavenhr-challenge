174be5817a621d8bd33af6556a16111d61045e7c
Dražen <drazen.buljovcic@gmail.com>
2018-01-05 19:34:45 +0100

    Slight code refactor.

--- src/components/FriendList.js
+++ src/components/FriendList.js
@@ -7,6 +7,15 @@ import PaginationControls from './PaginationControls';
 import styles from './FriendList.css';
 
 class FriendList extends Component {
+  //#region MARK: Super methods
+  constructor(props, context) {
+    super(props, context);
+
+    this.handlePaginationToNextPage = this.handlePaginationToNextPage.bind(this);
+    this.handlePaginationToPreviousPage = this.handlePaginationToPreviousPage.bind(this);
+    this.handlePaginationToPageNumber = this.handlePaginationToPageNumber.bind(this);
+  }
+
   componentWillMount() {
     this.props.refreshPaginationInfo(
       this.props.friendListInfo.friendsById,
@@ -22,6 +31,21 @@ class FriendList extends Component {
       nextProps.refreshPaginationInfo(nextFriendsList, nextProps.listItemCountPerPage);
     }
   }
+  //#endregion
+
+  //#region MARK: Pagination navigation handlers
+  handlePaginationToNextPage() {
+    this.props.changePaginationPage(this.props.friendListInfo.paginationInfo.currentPage + 1);
+  }
+
+  handlePaginationToPreviousPage() {
+    this.props.changePaginationPage(this.props.friendListInfo.paginationInfo.currentPage - 1);
+  }
+
+  handlePaginationToPageNumber(pageNumber) {
+    this.props.changePaginationPage(pageNumber);
+  }
+  //#endregion
 
   render () {
     const { friendsById, paginationInfo } = this.props.friendListInfo;
@@ -48,7 +72,9 @@ class FriendList extends Component {
         <PaginationControls
           totalPages={paginationInfo.totalPages}
           currentPage={paginationInfo.currentPage}
-          changePaginationPage={this.props.changePaginationPage}
+          handlePaginationToNextPage={this.handlePaginationToNextPage}
+          handlePaginationToPreviousPage={this.handlePaginationToPreviousPage}
+          handlePaginationToPageNumber={this.handlePaginationToPageNumber}
         />
       </div>
     );
--- src/components/PaginationControls.js
+++ src/components/PaginationControls.js
@@ -2,66 +2,38 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { times } from 'lodash';
 
-export default class PaginationControls extends React.Component {
-  //#region MARK: Super Methods
-  constructor(props, context) {
-    super(props, context);
-
-    this.handlePaginationToNextPage = this.handlePaginationToNextPage.bind(this);
-    this.handlePaginationToPreviousPage = this.handlePaginationToPreviousPage.bind(this);
-    this.handlePaginationToPageNumber = this.handlePaginationToPageNumber.bind(this);
-  }
-  //#endregion
-
-  //# MARK: Pagination navigation handlers
-  handlePaginationToNextPage() {
-    const { currentPage, changePaginationPage } = this.props;
-
-    changePaginationPage(currentPage + 1);
-  }
-
-  handlePaginationToPreviousPage() {
-    const { currentPage, changePaginationPage } = this.props;
-
-    changePaginationPage(currentPage- 1);
-  }
-
-  handlePaginationToPageNumber(pageNumber) {
-    this.props.changePaginationPage(pageNumber);
-  }
-  //#endregion
-
-  render() {
-    return (
-      <div>
-        <ul className="pagination">
-          <li>
-            <button href="#" className="btn btn-default" aria-label="Previous" onClick={this.handlePaginationToPreviousPage}>
-              <span aria-hidden="true">&laquo;</span>
+const PaginationControls = props => (
+  <div>
+    <ul className="pagination">
+      <li>
+        <button href="#" className="btn btn-default" aria-label="Previous" onClick={props.handlePaginationToPreviousPage}>
+          <span aria-hidden="true">&laquo;</span>
+        </button>
+      </li>
+      {
+        times(props.totalPages, (i) => (
+          <li key={i}>
+            <button href="#" className="btn btn-default" onClick={props.handlePaginationToPageNumber.bind(null, i)}>
+              {i + 1}
             </button>
           </li>
-          {
-            times(this.props.totalPages, (i) => (
-              <li key={i}>
-                <button href="#" className="btn btn-default" onClick={this.handlePaginationToPageNumber.bind(null, i)}>
-                  {i + 1}
-                </button>
-              </li>
-            ))
-          }
-          <li>
-            <button href="#" className="btn btn-default" aria-label="Next" onClick={this.handlePaginationToNextPage}>
-              <span aria-hidden="true">&raquo;</span>
-            </button>
-          </li>
-        </ul>
-      </div>
-    );
-  }
-}
+        ))
+      }
+      <li>
+        <button href="#" className="btn btn-default" aria-label="Next" onClick={props.handlePaginationToNextPage}>
+          <span aria-hidden="true">&raquo;</span>
+        </button>
+      </li>
+    </ul>
+  </div>
+);
 
 PaginationControls.propTypes = {
   totalPages: PropTypes.number.isRequired,
   currentPage: PropTypes.number.isRequired,
-  changePaginationPage: PropTypes.func.isRequired,
+  handlePaginationToNextPage: PropTypes.func.isRequired,
+  handlePaginationToPreviousPage: PropTypes.func.isRequired,
+  handlePaginationToPageNumber: PropTypes.func.isRequired,
 }
+
+export default PaginationControls;
