8ee9fafbc2945192ad5f533f4027790dfa090c60
Dražen <drazen.buljovcic@gmail.com>
2018-01-07 17:06:37 +0100

    Removed redux logger from dependencies.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,13 @@
 import React, { Component } from 'react';
 
-import { applyMiddleware, combineReducers, createStore } from 'redux';
+import { combineReducers, createStore } from 'redux';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger';
 
 import FriendListApp from './FriendListApp';
 import * as reducers from '../reducers';
 
 const reducer = combineReducers(reducers);
-const store = createStore(reducer, applyMiddleware(logger));
+const store = createStore(reducer);
 
 export default class App extends Component {
   render() {
